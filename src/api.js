4d1d7a7d22d2273aad6b5e25cdeaa1983d86e0c1
eberge <eberge@eberges-MacBook-Pro.local>
2020-10-12 17:34:37 -0400

    redo

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 export function fetchFollow(){
-    return fetch('api/followers').then((response) => {
+    return fetch('/api/followers').then((response) => {
         return response.json();
     })
        
 }
 
 export function deleteFollow(id){
-    return fetch(`api/followers/${id}`, {
+    return fetch(`/api/followers/${id}`, {
         method: "DELETE",
     });
 }
 
 export function saveFollow(data){
-    return fetch(`api/followers`,{
+    return fetch(`/api/followers`,{
         method: "POST",
         body: JSON.stringify(data),
         headers: {
